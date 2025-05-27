63b80b1122a33044d9bcedc4c760e1cdcdaf2789
Israel Falcão <israel.jesus@ba.estudante.senai.br>
2025-05-27 15:38:49 -0300

    Add files via upload

--- src/components/FormularioCadastro/index.js
+++ src/components/FormularioCadastro/index.js
@@ -7,26 +7,26 @@ import logo from '../../assets/images/logo.png';
 import axios from 'axios';
 
 function FormularioCadastro() {
-    const [nome, setNome] = useState('');
+    const [username, setUserName] = useState('');
     const [sexo, setSexo] = useState('');
-    const [idade, setIdade] = useState('');
-    const [altura, setAltura] = useState('');
-    const [peso, setPeso] = useState('');
-    const [posicao, setPosicao] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [confSenha, setConfSenha] = useState('');
+    const [nomeCompleto, setNomeCompleto] = useState('');
     const [numeroDaCamisa, setNumeroDaCamisa] = useState('');
     const navigate = useNavigate();
     const { exibirMensagem , mensagem, tipoMensagem, visivel, fecharMensagem } = useMensagem();
 
     const cadastrarUsuario = async () => {
         try {
-            const response = await axios.post('https://back-deploy-7q2n.onrender.com/usuarios', {nome, sexo, idade, altura, peso, posicao, numeroDaCamisa});
+            const response = await axios.post('https://back-deploy-7q2n.onrender.com/usuarios', {username, sexo, email, senha, confSenha, nomeCompleto, numeroDaCamisa});
             exibirMensagem(response.data.mensagem || 'Usuário cadastrado com sucesso!', 'sucesso');
-            setNome('');
+            setUserName('');
             setSexo('');
-            setIdade('');
-            setAltura('');
-            setPeso('');
-            setPosicao('');
+            setEmail('');
+            setSenha('');
+            setConfSenha('');
+            setNomeCompleto('');
             setNumeroDaCamisa('');
             navigate('/usuarios'); // Navegar para a lista de usuários após o cadastro
         } catch (error) {
@@ -44,83 +44,50 @@ function FormularioCadastro() {
     return (
         <div className="container">
             <img src={logo} alt="Logo da empresa" />
-            <h2>Cadastro de Jogadores</h2>
+            <h2>Bem vindo! Cadastre seus dados</h2>
             <form onSubmit={(e) => {e.preventDefault(); cadastrarUsuario()}}>
                 <input 
                     type="text"
-                    id="nome"
-                    placeholder="Nome"
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                    required
-                />
-                <input 
-                    type="number"
-                    id="idade"
-                    placeholder="Idade"
-                    value={idade}
-                    onChange={(e) => setIdade(e.target.value)}
+                    id="username"
+                    placeholder="Nome de Usuario"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
                     required
                 />
                 <input 
                     type="text"
-                    id="altura"
-                    placeholder="Altura"
-                    value={altura}
-                    onChange={(e) => setAltura(e.target.value)}
+                    id="nomeCompleto"
+                    placeholder="Nome Completo"
+                    value={nomeCompleto}
+                    onChange={(e) => setNomeCompleto(e.target.value)}
                     required
-                />
+                />       
                 <input 
                     type="text"
-                    id="peso"
-                    placeholder="Peso"
-                    value={peso}
-                    onChange={(e) => setPeso(e.target.value)}
+                    id="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <input 
-                    type="text"
-                    id="posicao"
-                    placeholder="Posição"
-                    value={posicao}
-                    onChange={(e) => setPosicao(e.target.value)}
+                    type="password"
+                    id="senha"
+                    placeholder="Senha"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
                     required
                 />
                 <input 
-                    type="number"
-                    id="numeroDaCamisa"
-                    placeholder="Numero da Camisa"
-                    value={numeroDaCamisa}
-                    onChange={(e) => setNumeroDaCamisa(e.target.value)}
+                    type="password"
+                    id="confSenha"
+                    placeholder="Confirmar Senha"
+                    value={confSenha}
+                    onChange={(e) => setConfSenha(e.target.value)}
                     required
                 />
-                <div className="sexo-container">
-                    <label>Sexo</label>
-                    <label>
-                        <input
-                            type="radio"
-                            id="masculino"
-                            name="sexo"
-                            value="MASCULINO"
-                            checked={sexo === "MASCULINO"}
-                            onChange={(e) => setSexo(e.target.value)}
-                            required
-                        />
-                        Masculino
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            id="feminino"
-                            name="sexo"
-                            value="FEMININO"
-                            checked={sexo === "FEMININO"}
-                            onChange={(e) => setSexo(e.target.value)}
-                            required
-                        />
-                        Feminino
-                    </label>
-                </div>
+                
+               
                 
                 <button type="submit">Cadastrar</button>
             </form>
--- src/pages/PaginaHome/index.js
+++ src/pages/PaginaHome/index.js
@@ -1,25 +1,28 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './styles.css';
 
-function PaginaHome() {  // Aqui, o nome do componente deve ser PaginaHome
+function PaginaHome() {
     const navigate = useNavigate();
 
     return (
         <div className='pagina-home'>
             <div className='home-container'>
                 <h1>My ToDo Login</h1>
-                
+
                 <input type="text" placeholder='Login'/>
                 <input type="password" placeholder='Password'/>
+
                 <button onClick={() => navigate('/usuarios')} className='telade login'>
                     Login
                 </button>
-                <button onClick={() => navigate('/cadastro')} className='cadastrar-novo-usuario'>
-                    Cadastrar Novo usuario
-                </button>
+
+                <Link to="/cadastro" className='cadastrar-novo-usuario'>
+                    Cadastrar Novo Usuário
+                </Link>
+
             </div>
         </div>
     );
 }
 
-export default PaginaHome;  // Corrigir o nome aqui também para exportar PaginaHome
+export default PaginaHome;
